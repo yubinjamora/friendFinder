3eb671d797a6dd302c05311aa55ecedc9791d958
Yubin Jamora <yubinjamora@Yubins-MacBook-Pro.local>
2017-11-21 12:03:49 -0500

    hw

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -37,24 +37,23 @@ module.exports = function(app) {
         var currentFriendScore = currentFriend.scores[j];
         var currentUserScore = userScores[j];
 
-        // We calculate the difference between the scores and sum them into the totalDifference
+        
         totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
       }
 
-      // If the sum of differences is less then the differences of the current "best match"
+     
       if (totalDifference <= bestMatch.friendDifference) {
-        // Reset the bestMatch to be the new friend.
+  
         bestMatch.name = currentFriend.name;
         bestMatch.photo = currentFriend.photo;
         bestMatch.friendDifference = totalDifference;
       }
     }
 
-    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-    // the database will always return that the user is the user's best friend).
+ 
     friends.push(userData);
 
-    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+  
     res.json(bestMatch);
   });
 };
